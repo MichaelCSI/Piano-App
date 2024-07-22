36cf256b00210c53c74adcf77312e0ab7f5709fe
Michael O’Sullivan <michaelosull@protonmail.ch>
2024-07-22 00:49:18 -0400

    Changes for production

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const bodyParser = require("body-parser");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 // Use CORS - allows requests from localhost:8080 (web) to localhost:3000 (server)
 app.use(cors());
